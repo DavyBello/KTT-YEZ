a77afb0ad68f5872b3e7ce5e73ce4f7232f4338f
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-05-10 23:08:28 +0100

    create manager portal

--- components/withCandidatePortal.js
+++ components/withCandidatePortal.js
@@ -67,6 +67,9 @@ export default function withLayout(Child, opts) {
               }}>
               {/* <Breadcrumb/> */}
               <Container fluid>
+                <p className="display-4 text-center" style={{fontSize: '2rem'}}>
+                  KTT YEZ Candidate Portal
+                </p>
                 <Child />
               </Container>
             </main>
--- components/withCompanyPortal.js
+++ components/withCompanyPortal.js
@@ -65,6 +65,9 @@ export default function withLayout(Child, opts) {
               <main className="main" style={{paddingTop: '24px'}}>
                 {/* <Breadcrumb/> */}
                 <Container fluid>
+                  <p className="display-4 text-center" style={{fontSize: '2rem'}}>
+                    KTT YEZ Company Portal
+                  </p>
                   <Child {...this.props}/>
                 </Container>
               </main>
